aa1198e2b6bf75fd3845367447e6df1f7c15b376
thepicture <85333076+thepicture@users.noreply.github.com>
2022-08-17 18:01:34 +0500

    feat(Piano): add sound on click

--- src/components/Piano/PianoKey/PianoKey.tsx
+++ src/components/Piano/PianoKey/PianoKey.tsx
@@ -4,29 +4,37 @@ import "./PianoKey.css";
 
 interface PianoKeyProps {
   withSemitone?: boolean;
+  cents: number;
+  onHold: (cents: number) => void;
+  onRelease: () => void;
 }
 
-export const PianoKey: React.FC<PianoKeyProps> = ({ withSemitone }) => {
-  return withSemitone ? (
+export const PianoKey: React.FC<PianoKeyProps> = ({
+  withSemitone,
+  cents,
+  onHold,
+  onRelease,
+}) => {
+  return (
     <div className="PianoKey__container">
       <button
+        onMouseDown={() => onHold(cents)}
+        onMouseUp={onRelease}
+        onMouseLeave={onRelease}
         type="button"
         title="piano key"
         className="PianoKey PianoKey--white"
       />
-      <button
-        type="button"
-        title="piano key"
-        className="PianoKey PianoKey--black"
-      />
-    </div>
-  ) : (
-    <div className="PianoKey__container">
-      <button
-        type="button"
-        title="piano key"
-        className="PianoKey PianoKey--white"
-      />
+      {withSemitone && (
+        <button
+          onMouseDown={() => onHold(cents + 100)}
+          onMouseUp={onRelease}
+          onMouseLeave={onRelease}
+          type="button"
+          title="piano key"
+          className="PianoKey PianoKey--black"
+        />
+      )}
     </div>
   );
 };
