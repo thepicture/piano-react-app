6f9513f9aa01ff90547b7fef1a9914db99e70fef
thepicture <85333076+thepicture@users.noreply.github.com>
2022-08-18 16:47:41 +0500

    feat(Piano): add keyboard play from z to m

--- src/components/SynthesizerBody/Piano/Piano.tsx
+++ src/components/SynthesizerBody/Piano/Piano.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 import "./Piano.css";
 
@@ -7,6 +7,20 @@ import { PianoKey } from ".";
 const OCTAVES_COUNT = 2;
 const START_CENTS = 300;
 const INDEXES_WITH_SEMITONE = [0, 1, 3, 4, 5];
+const KEYS_MAPPING = [
+  "z",
+  "s",
+  "x",
+  "d",
+  "c",
+  "v",
+  "g",
+  "b",
+  "h",
+  "n",
+  "j",
+  "m",
+];
 
 interface PianoProps {
   onHold: (cents: number) => void;
@@ -14,9 +28,24 @@ interface PianoProps {
 }
 
 export const Piano: React.FC<PianoProps> = ({ onHold, onRelease }) => {
-  let keys = [] as any[];
+  let keys: any[] = [];
 
   let cents = START_CENTS;
+
+  useEffect(() => {
+    let keyCents = START_CENTS;
+
+    for (let i = 0; i < 7 * OCTAVES_COUNT; ++i) {
+      (function (index, cents) {
+        document.addEventListener(
+          "keydown",
+          (event) => event.key === KEYS_MAPPING[index] && onHold(cents)
+        );
+        document.addEventListener("keyup", () => onRelease());
+      })(i, keyCents);
+      keyCents += 100;
+    }
+  }, [onHold, onRelease]);
 
   for (let i = 0; i < 7 * OCTAVES_COUNT; ++i) {
     const isWithSemitone = INDEXES_WITH_SEMITONE.includes(i % 7);
@@ -32,6 +61,7 @@ export const Piano: React.FC<PianoProps> = ({ onHold, onRelease }) => {
         />
       );
     })(cents);
+
     if (isWithSemitone) {
       cents += 200;
     } else {
