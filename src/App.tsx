7db00628ceb91c05b7af3459153fa735067298d0
thepicture <85333076+thepicture@users.noreply.github.com>
2022-08-19 14:33:44 +0500

    style(App): add 3d perspective

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { SynthesizerBody } from "./components";
 function App() {
   return (
     <main className="App">
-      <section className="App__synthesizer-body">
+      <section className="App__synthesizer-body App__synthesizer-body--3d">
         <SynthesizerBody octavesCount={3} startCents={300 - 12 * 100} />
       </section>
     </main>
